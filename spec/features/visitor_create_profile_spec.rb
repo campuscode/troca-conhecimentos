c094aa7f132d44f3c64e741bf990d381650c4ecb
Felipe e Rogério <flaprano@gmail.com>
2017-07-21 21:11:52 -0300

    applying bootstrap

--- spec/features/visitor_create_profile_spec.rb
+++ spec/features/visitor_create_profile_spec.rb
@@ -5,10 +5,12 @@ feature 'Visitor create account' do
     visit root_path
     click_on 'Cadastrar'
 
-    fill_in 'Email', with: 'teste@teste.com'
-    fill_in 'Senha', with: '123456'
-    fill_in 'Confirme a senha', with: '123456'
-    click_on 'Cadastrar'
+    within('form') do
+      fill_in 'Email', with: 'teste@teste.com'
+      fill_in 'Senha', with: '123456'
+      fill_in 'Confirme sua senha', with: '123456'
+      click_on 'Cadastrar'
+    end
 
     expect(current_path).to eq root_path
     expect(page).to have_css('h2', text: 'Você ainda nao completou o seu perfil, Clique aqui para continuar')
