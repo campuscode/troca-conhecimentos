4fbabb66f0e72ec4405c7da10082bdb17bdf9273
Elizabeth <elizabethlopesramos@gmail.com>
2017-07-24 16:25:16 -0300

    Adiciona ad_id ao proposal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723153134) do
+ActiveRecord::Schema.define(version: 20170724192329) do
 
   create_table "ads", force: :cascade do |t|
     t.string "requested_knowledge"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20170723153134) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "ad_id"
+    t.index ["ad_id"], name: "index_proposals_on_ad_id"
     t.index ["user_id"], name: "index_proposals_on_user_id"
   end
 
