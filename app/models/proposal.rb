5a24936ecb520d2259b32d371792fc464804a6b1
Joao e Reinaldo <jhm.vale@gmail.com>
2017-07-24 22:05:23 -0300

    adding model proposal

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,7 +1,6 @@
 class Proposal < ApplicationRecord
     validates :description, :requested_knowledge, :email, :day_period,
               :meeting_type, presence: {message: 'Houve um erro ao tentar enviar a proposta'}
-    enum status: [:pending, :approved , :rejected]
     belongs_to :user
     belongs_to :ad
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_one :profile
   has_many :proposals
   has_many :ad
+  has_many :proposal
 end
