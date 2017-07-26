ff7be54f17e3e5da632e5b2a495bd95da0f8373a
Eliza e Lucas <elizabethlopesramos@gmail.com>
2017-07-25 21:15:08 -0300

    user see only your proposals

--- spec/features/user_see_your_proposals_spec.rb
+++ spec/features/user_see_your_proposals_spec.rb
@@ -36,4 +36,52 @@ feature 'user list his proposals' do
     expect(page).to have_css('dl', text: other_proposal.status)
     #expectativa
   end
+
+  scenario 'user has no proposals' do
+    user = create(:user)
+    profile = create(:profile, user: user)
+    login_as(user)
+
+    visit root_path
+
+    click_on 'Minhas propostas'
+
+    expect(page).to have_css('h1', text: 'Voce nao tem propostas cadastradas')
+  end
+
+  scenario 'user oly see your proposals' do
+    user = create(:user)
+    profile = create(:profile, user: user)
+    ad = create(:ad, requested_knowledge: 'Quero aprender Ruby on Rails!')
+    proposal = create(:proposal, user: user, ad: ad, description: 'Sei tocar violao',
+                      requested_knowledge: 'Quero aprender a cozinhar', email: 'eliza@teste.com',
+                      day_period: 'Manha', meeting_type: 'Online', status: :approved)
+
+    another_user = create(:user)
+    another_profile = create(:profile, user: another_user)
+    another_ad = create(:ad, requested_knowledge: 'Quero aprender java!')
+    another_proposal = create(:proposal, user: another_user, ad: another_ad, description: 'Sei Jogar Bola',
+                      requested_knowledge: 'quero aprender violao', email: 'test@teste.com',
+                      day_period: 'Tarde', meeting_type: 'presencial', status: :pending)
+
+    login_as(user)
+
+    visit root_path
+
+    click_on 'Minhas propostas'
+
+
+    expect(page).to have_css('dl', text: proposal.requested_knowledge)
+    expect(page).to have_css('dl', text: proposal.email)
+    expect(page).to have_css('dl', text: proposal.day_period)
+    expect(page).to have_css('dl', text: proposal.meeting_type)
+    expect(page).to have_css('dl', text: proposal.status)
+
+
+    expect(page).not_to have_css('dl', text: another_proposal.requested_knowledge)
+    expect(page).not_to have_css('dl', text: another_proposal.email)
+    expect(page).not_to have_css('dl', text: another_proposal.day_period)
+    expect(page).not_to have_css('dl', text: another_proposal.meeting_type)
+    expect(page).not_to have_css('dl', text: another_proposal.status)
+  end
 end
