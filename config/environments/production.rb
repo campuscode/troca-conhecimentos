e06cd7b17ce5bf84c110298f2d42e7ccfe26c4ea
Joao Almeida <joaorsalmeida@gmail.com>
2017-07-28 19:03:12 -0300

    ADd send grid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,17 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
